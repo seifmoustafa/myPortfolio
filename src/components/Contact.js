72a9bc98221c695d753a312592379249fec9f898
Seif Moustafa <seif.moustafa516@gmail.com>
2024-11-24 20:25:07 +0200

    "Added Font Awesome icons to Contact component, updated Contact.js and Contact.css files, and modified package.json and package-lock.json files to include new dependencies."

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,57 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { fadeIn } from "../animations";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import "../components/styles/Contact.css";
 
 const Contact = () => {
   return (
     <section id="contact" className="contact">
-      <motion.div className="container" initial="hidden" animate="visible" variants={fadeIn}>
+      <motion.div
+        className="container"
+        initial="hidden"
+        animate="visible"
+        variants={{
+          hidden: { opacity: 0 },
+          visible: { opacity: 1, transition: { duration: 1 } },
+        }}
+      >
         <h2>Contact Me</h2>
-        <p>Email: <a href="mailto:seif.moustafa516@gmail.com">seif.moustafa516@gmail.com</a></p>
-        <p>LinkedIn: <a href="https://www.linkedin.com/in/seif-moustafa-60115f/" target="_blank" rel="noopener noreferrer">Seif Moustafa</a></p>
-        <p>
-          Support Me: <a href="https://buymeacoffee.com/seifmoustafa" target="_blank" rel="noopener noreferrer">
-            Buy Me a Coffee
+        <div className="contact-icons">
+          <a
+            href="mailto:seif.moustafa516@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="icon-container"
+          >
+            <FontAwesomeIcon icon={faEnvelope} className="icon" />
           </a>
-        </p>
+          <a
+            href="https://www.linkedin.com/in/seif-moustafa-60115f/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="icon-container"
+          >
+            <FontAwesomeIcon icon={faLinkedin} className="icon" />
+          </a>
+        </div>
+
+        {/* Support Me Section */}
+        <div className="support-me">
+          <a
+            href="https://buymeacoffee.com/seifmoustafa"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="coffee-button"
+          >
+            <img
+              src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+              alt="Buy Me a Coffee"
+              className="coffee-icon"
+            />
+          </a>
+        </div>
       </motion.div>
     </section>
   );
